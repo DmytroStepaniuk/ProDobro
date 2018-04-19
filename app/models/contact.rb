da760e04aa6e836b8950a542a28959c038ef6eeb
mm1krob <mmikrob@gmail.com>
2018-04-20 00:13:48 +0300

    develop_PROD-83: add contact model, add address model, add specs

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,5 +5,4 @@ class Contact < ApplicationRecord
   validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/ },
                     presence: true
   validates :phone, format: { with: /\A[+]\d{2}-\d{3}-\d{3}-\d{2}-\d{2}\z/ },
-                    presence: true
 end
