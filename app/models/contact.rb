02a04c0429d759ddaa1f11e087054870117a258f
mm1krob <mmikrob@gmail.com>
2018-05-04 15:01:19 +0300

    develop_PROD-83: add edit contacts view, controller, models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,10 @@
 class Contact < ApplicationRecord
   has_one :address
+
   accepts_nested_attributes_for :address
 
   validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/ },
                     presence: true
   validates :phone, format: { with: /\A[+]\d{2}-\d{3}-\d{3}-\d{2}-\d{2}\z/ },
+                    presence: true
 end
